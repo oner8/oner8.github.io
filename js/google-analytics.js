21d8763ed753591bb79c0885cfbb55e7ec299c3d
oner8 <realdannyw@hotmail.com>
2020-03-03 03:26:18 +0800

    Site updated: 2020-03-03 03:26:16

--- js/google-analytics.js
+++ js/google-analytics.js
@@ -10,5 +10,5 @@
     m.parentNode.insertBefore(a, m)
 })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
-ga('create', 'UA-88888888-1', 'auto');
+ga('create', 'UA-97044633-1', 'auto');
 ga('send', 'pageview');
